c6909a520057cd84cd92e430457b33fad6b3eb67
l-yeong <yahn10@naver.com>
2025-09-02 15:31:36 +0900

    alert-> send, onmessage 추가

--- src/main/resources/static/실습/실습2/실습2.js
+++ src/main/resources/static/실습/실습2/실습2.js
@@ -2,10 +2,15 @@ console.log("실습2 open");
 
 const alarm = new WebSocket("/alarm");
 
-alarm.onopen = (event) =>{
-    alert("익명의 유저가 접속 했습니다.")
+alarm.onopen = () =>{
+    alarm.send("익명의 유저가 접속 했습니다.")
 }//func end
 
-alarm.onclose = (event) =>{
-    alert("익명의 유저가 퇴장 했습니다.")
+alarm.onmessage =()=>{
+    alert(event.data);
 }//func end
+
+alarm.onclose = () =>{
+    alarm.send("익명의 유저가 퇴장 했습니다.")
+}//func end
+
