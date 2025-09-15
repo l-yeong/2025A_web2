cd75f74af172b79d9b6b004f43399fd6657b19f6
l-yeong <yahn1204@gmail.com>
2025-09-15 17:40:56 +0900

    마이바티스_1일차_예제

--- src/main/java/example/day06/BatisController.java
+++ src/main/java/example/day06/BatisController.java
@@ -1,8 +1,6 @@
 package example.day06;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
