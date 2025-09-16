13823f13fe95a55beb3f35ec0b780bbbe1e14c52
l-yeong <yahn1204@gmail.com>
2025-09-16 10:56:21 +0900

    커밋

--- src/main/reactapp/src/App.jsx
+++ src/main/reactapp/src/App.jsx
@@ -2,10 +2,12 @@ function App() {
 
   return (
     <>
-        <h1> 여기가 최초 렌더링 합니다. </h1>
+        <h1> 여기가 최초 렌더링 합니다.App.jsx </h1>
         
     </>
   )
 }
 
+
+// 다른 js 파일에서 함수를 import 할수 있도록 export
 export default App
