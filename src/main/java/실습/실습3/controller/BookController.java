89499dce4b22aae52ff10383858857385d4b3692
l-yeong <yahn1204@gmail.com>
2025-09-26 15:00:36 +0900

    CRUD_완료

--- src/main/java/실습/실습3/controller/BookController.java
+++ src/main/java/실습/실습3/controller/BookController.java
@@ -1,27 +1,55 @@
 package 실습.실습3.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import 실습.실습3.service.BookService;
 
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("/book")
 @RequiredArgsConstructor
+@Log4j2 // 로그(기록) 처리하는 어노테이션 제공
 public class BookController {
     private final BookService bookService;
 
+    // 2. print 함수 대신에 로그함수 사용해보기 = 부가기능 제공, 제약 조건 등
+    @GetMapping("/log")
+    public void log(){
+        System.out.println("개발단계에서는 print 정말 많이 작성하자...");
+        //log.XXX : 출력함수 처럼 출력하는 메시지 함수 이면서 부가기능(파일처리/제약조건)
+        log.debug("테스트 과정 사용된다.");      // 테스트(개발)과정 에서 메시지
+        log.info("서비스의 흐름/상태 사용된다");  // 운용과정 메시지
+        log.warn("잠재적 문제 사용된다.");       // 유지보수 과정 메시지
+        log.error("예외/실패 상황 사용된다.");    // 운용/유지보수 과정 메시지
+    }//func end
+
+
     @PostMapping("/rent")
-    public boolean borrowBook(@RequestBody Map<String,Object>bookList){
-        return bookService.borrowBook(bookList);
+    public ResponseEntity<Boolean> borrowBook(@RequestBody Map<String,Object>bookList){
+        boolean result = false;
+        try{
+            result = bookService.borrowBook(bookList);
+            log.debug("[대여성공]"+bookList);
+            return ResponseEntity.ok(result);
+        } catch (RuntimeException e) {
+            log.debug("[대여실패]"+e.getMessage()); //만약에 롤백이면 e.getMessage():예외메시지
+            return ResponseEntity.status(405).body(result);
+        }
+
     }//func end
 
     @PostMapping("/return")
-    public boolean returnBook(@RequestBody Map<String,Object>bookListReturn){
-        return bookService.returnBook(bookListReturn);
+    public ResponseEntity<Boolean> returnBook(@RequestBody Map<String,Object>bookListReturn){
+        boolean result = false;
+        try{
+            result = bookService.returnBook(bookListReturn);
+            return ResponseEntity.ok(result);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(405).body(result);
+        }
     }
 }//class end
--- src/main/java/실습/실습3/model/mapper/BookMapper.java
+++ src/main/java/실습/실습3/model/mapper/BookMapper.java
@@ -6,18 +6,18 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface BookMapper {
     // 책 재고 차감
-    @Insert("update books set stock = stock - #{stock} where title=#{title} and stock > 0")
-    public boolean borrowBook(int stock, String title);
+    @Insert("update books set stock = stock-1 where id=#{bookid} and stock > 0")
+    public int borrowBook(int bookid);
 
     //책 대여 기록
-    @Insert("INSERT INTO rentals(book_id,member) values(#{book_id},#{member});")
-    public boolean borrowBookLog(int book_id, String member);
+    @Insert("INSERT INTO rentals(book_id,member) values(#{bookid},#{member});")
+    public int borrowBookLog(int bookid, String member);
 
     // 책 재고 증가
-    @Insert("update books set stock = stock + #{stock} where title=#{title}")
-    public boolean returnBook(int stock, String title);
+    @Insert("update books set stock = stock +1 where id=#{bookid}")
+    public int returnBook(int bookid);
 
     // 책 반납 기록
-    @Insert("INSERT INTO rentals(book_id,member,return_date) values(#{book_id},#{member},now())")
-    public boolean returnBookLog(int book_id, String member);
+    @Insert("INSERT INTO rentals(book_id,member,return_date) values(#{bookid},#{member},now())")
+    public int returnBookLog(int bookid, String member);
 }//inter end
