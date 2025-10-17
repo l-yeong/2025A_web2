7bce7f07377ca042d5263830d8a21d349b6f1456
l-yeong <yahn1204@gmail.com>
2025-10-17 13:42:01 +0900

    redis 커밋

---
+++ src/main/java/example/day17/StudentDto.java
@@ -0,0 +1,16 @@
+package example.day17;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class StudentDto {
+        private int sno;
+        private String name;
+        private int kor;
+        private int math;
+
+}//class end1
