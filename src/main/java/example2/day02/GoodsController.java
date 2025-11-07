f54c13f62ac4e584e348696db1affdb5759b0b4d
l-yeong <yahn10@naver.com>
2025-11-07 15:10:57 +0900

    flutter 3일차_cors전역변수추가

--- src/main/java/example2/day02/GoodsController.java
+++ src/main/java/example2/day02/GoodsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/goods")
+@CrossOrigin(origins = "*")
 public class GoodsController {
     private final GoodsService goodsService;
 
